5e3b083d6ec4cf2f1976060079181d0d2b322d74
Nekai <alessandrobelotti4570@gmail.com>
2026-02-07 11:26:24 +0100

    Add setting for the preferred language.

--- NekaiManager/Data/DTOs/SettingsDto.cs
+++ NekaiManager/Data/DTOs/SettingsDto.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using Nekai.Common;
 
 namespace NekaiManager.Data.DTOs;
 
 public class SettingsDto
 {
 	public bool PreferDarkMode { get; set; }
+	public DisplayLanguage PreferredLanguage { get; set; }
 }
